6892c5476ebbf3568b95ddcd2cf0db40f5d8cea8
Rob Lathrop <rl@polyswarm.io>
2019-05-20 09:50:44 -0400

    Update setup like other projects

--- setup.py
+++ setup.py
@@ -1,23 +1,39 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-try:
-    long_description = open("README.rst").read()
-except IOError:
-    long_description = ""
+
+def parse_requirements():
+    with open('requirements.txt', 'r') as f:
+        return [r for r in f.read().splitlines() if not r.startswith('git') and not r.startswith('.')]
+
+
+# The README.md will be used as the content for the PyPi package details page on the Python Package Index.
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
 
 setup(
     name="polyswarm-artifact",
     version="0.1.0",
     description="Library containing artifact type enums and functions",
-    license="MIT",
-    author="PolySwarm Developers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='PolySwarm Developers',
+    author_email='info@polyswarm.io',
+    url='https://github.com/polyswarm/polyswarm-artifact',
+    license='MIT',
+    python_requires='>=3.6.5,<4',
+    install_requires=parse_requirements(),
+    include_package_data=True,
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    install_requires=[],
-    long_description=long_description,
     classifiers=[
-        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: PyPy",
     ]
 )

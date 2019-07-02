d9e6e0032dedeb49b5ba97337fe04d2095d8ed6d
Rob Lathrop <rl@polyswarm.io>
2019-07-02 13:03:49 -0400

    Add option to silence exception output in Schema.validate()

--- src/polyswarmartifact/schema/schema.py
+++ src/polyswarmartifact/schema/schema.py
@@ -28,11 +28,12 @@ class Schema(ABC):
         raise NotImplementedError('json is not implemented')
 
     @classmethod
-    def validate(cls, value, resolver=None):
+    def validate(cls, value, resolver=None, silent=False):
         """
         Validates a JSON value against the schema
         :param value: Dict or array to compare
         :param resolver: Optional resolver for finding references
+        :param silent: Silence exception if silent is True
         :return: Boolean, True if JSON matches the schema
         """
         schema_path, schema_name = cls.get_schema()
@@ -48,7 +49,8 @@ class Schema(ABC):
         try:
             validate(instance=value, schema=schema, resolver=resolver)
         except ValidationError:
-            logger.exception('Failed to validate schema')
+            if not silent:
+                logger.exception('Failed to validate schema')
             return False
 
         return True

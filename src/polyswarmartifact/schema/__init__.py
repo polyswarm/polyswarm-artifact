c3b017333c57aae2574d2b7bf6c17ff1d65c5b45
Rob Lathrop <rl@polyswarm.io>
2021-06-09 15:11:44 -0400

    Changes Verdict to ScanMetadata
    This solves the conflict with mincroengine-webhooks-py Verdict enum
    
    Note, Verdict now inherits from ScanMetadata for backwards compat.

--- src/polyswarmartifact/schema/__init__.py
+++ src/polyswarmartifact/schema/__init__.py
@@ -1,7 +1,7 @@
 from .schema import Schema
 from .assertion import Assertion
 from .bounty import Bounty, FileArtifact, URLArtifact
-from .verdict import Verdict, Scanner, StixSignature
+from .verdict import Verdict, Scanner, StixSignature, ScanMetadata
 
 __all__ = [
     'Assertion',
